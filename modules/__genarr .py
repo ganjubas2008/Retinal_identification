b1594ee9bd628c8e0a58e283fdf32029ed1a0972
kxlll <79812895+kxlll@users.noreply.github.com>
2021-03-01 02:49:44 +0300

    Update __genarr .py

--- modules/__genarr .py
+++ modules/__genarr .py
@@ -5,8 +5,7 @@ from segm import *
 import json
 import numpy as np
 
-"""первые 10 наборов - поворот до 10, вторые 10 наборов - поворот до 20. нумерация с 1"""
-#this generator produces extra images using augmentation
+"""This generator produces extra images using augmentation."""
 messidor = "/MESSIDOR"
 
 xtr = []
@@ -28,17 +27,11 @@ def main():
         spam(vessels, i//2, xtr, ytr, k=70)
         spam(vessels, i//2, xtest, ytest, k=10)
 
-    tools.write_to_json(np.array(xtr), "xtr1", 1)
-    tools.write_to_json(np.array(ytr), "ytr1", 1)
-
-    tools.write_to_json(np.array(xtest), "xtest1", 1)
-    tools.write_to_json(np.array(ytest), "ytest1", 1)
+    return xtr, ytr, xtest, ytest
 
 def spam(img, key, x, y, k=70):
     dx = dy = 12
     dr = 15
-    #cv2.imwrite(f"{0} {int(0)} {int(0)} {round(0, 2)}gold.tif", img)
-    #if BOOLEAN: k = 4000
     for i in range(k):
         shift = (uniform(-dx, dx), uniform(-dy, dy))
         rot = (uniform(-dr, dr))
@@ -46,6 +39,4 @@ def spam(img, key, x, y, k=70):
         # tools.show(imgspam)
         x.append(imgspam)
         y.append(key)
-
-main()
 
